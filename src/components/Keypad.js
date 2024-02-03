e5fb51722469197a50e202e037c56c71a776d905
irene <irenejim9504@gmail.com>
2024-02-03 19:28:37 +0300

    used the handle event

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handleChange() {
+    console.log('Entering password...');
+    // Additional logic for handling the password input can be added here
+  }
+
+  return (
+    <div>
+      <label>Password:</label>
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
